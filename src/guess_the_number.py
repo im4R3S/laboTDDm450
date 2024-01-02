0764516e7db715137c238f4bcb65c56101120f22
cr1m31 <cr1m3man@gmail.com>
2024-01-02 20:34:39 +0100

    removed game call

--- src/guess_the_number.py
+++ src/guess_the_number.py
@@ -1,28 +1,3 @@
 def check_guess(number_to_guess, user_input):
     
     return 0
-
-
-
-
-# if you want to play the game (uncomment the code below)
-    
-"""
-
-number_to_guess = 42
-
-while True:
-    user_input = input("Enter your guess (or 'exit' to quit): ")
-
-    if user_input.lower() == 'exit':
-        print("Thanks for playing!")
-        break
-
-    result = check_guess(number_to_guess, user_input)
-    print(result)
-
-    if result == "Correct guess!":
-        print("Congratulations, you've guessed the number!")
-        break
-
-"""        
