03f8c1314b6c9c9564278f6d4991d0e4b30d8baf
cr1m31 <cr1m3man@gmail.com>
2024-01-14 19:15:09 +0100

    iteration 2 fixed first test by refactor

--- tests/test_guess_the_number.py
+++ tests/test_guess_the_number.py
@@ -6,17 +6,6 @@ from guess_the_number import check_guess
 def test_correct_guess():
     # Arrange 
     number_to_guess = 5
-    user_input = '5'
-
-    # Act
-    result = check_guess(number_to_guess, user_input)
-    
-    # Assert
-    assert result == user_input
-
-def test_guess_too_low():
-    # Arrange 
-    number_to_guess = 5
     user_input = 5
 
     # Act
