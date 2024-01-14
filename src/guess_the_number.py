502deed8cd620f202e27758a7312223be654702e
cr1m31 <cr1m3man@gmail.com>
2024-01-14 19:47:20 +0100

    iteration 4 refactor of the function

--- src/guess_the_number.py
+++ src/guess_the_number.py
@@ -1,12 +1,10 @@
 def check_guess(number_to_guess, user_input):
-    if type(user_input) == str:
-        try:
-            user_input = int(user_input)
-            if user_input < number_to_guess:
-                return "Too low"
-            elif user_input == number_to_guess:
-                return user_input
-        except ValueError:
-            return 0
-    else:
+    try:
+        user_input = int(user_input)
+        if user_input < number_to_guess:
+            return "Too low"
+        elif user_input == number_to_guess:
+            return user_input
+    except ValueError:
         return 0
+    return 0
