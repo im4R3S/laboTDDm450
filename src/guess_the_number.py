525f66cf0f870cf6b952873d94354564bab66293
cr1m31 <cr1m3man@gmail.com>
2024-01-17 21:25:12 +0100

    iteration 1 exo2 failing test valueerror and typeerror

--- src/guess_the_number.py
+++ src/guess_the_number.py
@@ -1,4 +1,24 @@
 def check_guess(number_to_guess, user_input):
+<<<<<<< Updated upstream
   
     return "Correct guess!"
  
+=======
+    try:
+        # user_input = int(user_input)
+        
+        if not isinstance(user_input, int):
+            raise TypeError('Work with Numbers Only')
+        if user_input < 0:
+            raise ValueError('Work with Positive Numbers Only')
+
+
+        if user_input < number_to_guess:
+            return "Too low"
+        elif user_input == number_to_guess:
+            return user_input
+    except ValueError:
+        return 0
+
+    return 0
+>>>>>>> Stashed changes
