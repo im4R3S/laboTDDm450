b29b1b54d7f500e94db56ee7aa42e1134e540963
cr1m31 <39967332+cr1m31@users.noreply.github.com>
2024-01-19 09:22:01 +0100

    iteration 3 exo2 refactor return values

--- src/guess_the_number.py
+++ src/guess_the_number.py
@@ -1,6 +1,5 @@
 def check_guess(number_to_guess, user_input):
     try:
-        # user_input = int(user_input)
         if user_input < 0:
             raise ValueError('Work with Positive Numbers Only')
 
@@ -8,6 +7,7 @@ def check_guess(number_to_guess, user_input):
             print("Too low")
             return user_input
         elif user_input == number_to_guess:
+            print("Correct")
             return user_input
     except ValueError:
         raise TypeError('Work with Numbers Only')
